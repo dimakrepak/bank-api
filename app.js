702429db9e68d450f1dffdbb92099cbca9aeb809
Dima <krepakdima@gmail.com>
2021-04-18 17:39:32 +0300

    final

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.PORT || 5000;
 const usersRoute = require('./routes/users.route');
 const cors = require('cors');
 
+const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
