0e0df04f924246a845b741d16a70c49e544806f8
Dima <krepakdima@gmail.com>
2021-04-18 16:28:27 +0300

    final

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 const usersRoute = require('./routes/users.route');
 const cors = require('cors');
 
@@ -10,4 +10,4 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/api/bank', usersRoute);
 
-app.listen(PORT, () => console.log(`application start at ${PORT}`))
+app.listen(port, () => console.log(`application start at ${port}`))
